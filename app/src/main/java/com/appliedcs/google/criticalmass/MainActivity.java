f9f6bf55e18dbb7dee1822ab4d24b3c7dc54f95b
Sandesh Shetty <sandeshshetty95@gmail.com>
2016-04-02 13:34:01 +0530

    OnGridItemClickListener implementation started

--- app/src/main/java/com/appliedcs/google/criticalmass/MainActivity.java
+++ app/src/main/java/com/appliedcs/google/criticalmass/MainActivity.java
@@ -2,14 +2,44 @@ package com.appliedcs.google.criticalmass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int NUM_TILES = 48;
+    private int NUM_COLUMNS = 8;
+
+    private int currentPlayerNumber = 0;
+
+    private TextView currentPlayerView;
+    private GridView criticalGridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // hello player1 here
+        criticalGridView = (GridView) findViewById(R.id.criticalGridView);
+        currentPlayerView = (TextView) findViewById(R.id.currentPlayerView);
+
+        criticalGridView.setNumColumns(NUM_COLUMNS);
+        criticalGridView.setAdapter(new TileAdapter(this, NUM_TILES));
+
+        currentPlayerView.setText("" + (currentPlayerNumber+1));
+
+        criticalGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                int x = position % NUM_COLUMNS;
+                int y = position % NUM_TILES;
+
+                Toast.makeText(MainActivity.this, position + " clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/appliedcs/google/criticalmass/TileAdapter.java
@@ -0,0 +1,58 @@
+package com.appliedcs.google.criticalmass;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+
+
+/**
+ * Created by Sandesh on 02-04-2016.
+ */
+public class TileAdapter extends BaseAdapter {
+
+    private Context context;
+    int numTiles;
+
+    public TileAdapter(Context context, int numTiles) {
+        this.context = context;
+        this.numTiles = numTiles;
+    }
+
+    @Override
+    public int getCount() {
+        return numTiles;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView textView;
+        if(convertView == null) {
+            textView = new TextView(context);
+            textView.setText("" + position);
+            textView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            textView.setBackgroundResource(R.color.black);
+//            textView.setPadding(8, 8, 8, 8);
+            textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+        } else {
+            textView = (TextView) convertView;
+        }
+        return textView;
+    }
+}
